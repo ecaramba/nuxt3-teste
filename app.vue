43d3d59a92dac53b05f11e473a90492fae51b57d
Edir Dumaszak <ecaramba@gmail.com>
2024-04-19 16:38:19 -0300

    config

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    {{ data.data }}
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+const data = await useFetch('/api/teste')
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    awsAmplify: {
+      // catchAllStaticFallback: true,
+      // imageOptimization: { "/_image", cacheControl: "public, max-age=3600, immutable" },
+      // imageSettings: { ... },
+    }
+  }
 })
